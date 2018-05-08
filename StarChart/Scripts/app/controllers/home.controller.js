1367c487a096583a033002cbed12a7e4ec37b570
Tiller <ctiller@proforma.com>
2018-05-08 17:16:31 -0400

    update w/ bare minimum required to get functional frontend

--- StarChart/Scripts/app/controllers/home.controller.js
+++ StarChart/Scripts/app/controllers/home.controller.js
@@ -1,3 +1,6 @@
 ﻿app.controller("homeController", ["$scope", "$http", function ($scope, $http) {
-	$scope.title = "hello";
+	//$scope.title = "hello";
+	$scope.subscriptions = ["a", "b", "c", "d", "e"];
+
+	$scope.products = ["f", "g", "h", "i", "j"];
 }]);
